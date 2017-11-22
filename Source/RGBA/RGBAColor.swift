dad38a6f2ce3f33ec1b7a651229a9d6bc32e16e5
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-11-22 04:44:52 +0300

    Converted RGBAColor from structure to class

--- Source/RGBA/RGBAColor.swift
+++ Source/RGBA/RGBAColor.swift
@@ -7,10 +7,71 @@
 //
 
 import Foundation
+import UIKit
 
-public struct RGBAColor {
-    public var red: RGBAComponent
-    public var green: RGBAComponent
-    public var blue: RGBAComponent
-    public var alpha: RGBAComponent
+public class RGBAColor {
+    
+    // MARK: Class variables & properties
+    
+    // MARK: Public class methods
+    
+    public class func from(color: UIColor) -> RGBAColor {
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 0.0
+        
+        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        return RGBAColor(
+            red: RGBAComponent(cgFloatValue: red),
+            green: RGBAComponent(cgFloatValue: green),
+            blue: RGBAComponent(cgFloatValue: blue),
+            alpha: RGBAComponent(cgFloatValue: alpha)
+        )
+    }
+    
+    // MARK: Private class methods
+    
+    // MARK: Initializers
+    
+    public init(red: RGBAComponent, green: RGBAComponent, blue: RGBAComponent, alpha: RGBAComponent) {
+        // Initialize red
+        
+        self.red = red
+        
+        // Initialize green
+        
+        self.green = green
+        
+        // Initialize blue
+        
+        self.blue = blue
+        
+        // Initialize alpha
+        
+        self.alpha = alpha
+    }
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    // MARK: Object variables & properties
+    
+    public fileprivate(set) var red: RGBAComponent
+    
+    public fileprivate(set) var green: RGBAComponent
+    
+    public fileprivate(set) var blue: RGBAComponent
+    
+    public fileprivate(set) var alpha: RGBAComponent
+    
+    // MARK: Public object methods
+    
+    // MARK: Private object methods
+    
+    // MARK: Protocol methods
+    
 }
