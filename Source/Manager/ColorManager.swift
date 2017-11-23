d913369f43d9ddc4aae25f01dd44cb30088991c0
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-11-23 06:04:05 +0300

    Updated ColorManager class

--- Source/Manager/ColorManager.swift
+++ Source/Manager/ColorManager.swift
@@ -18,10 +18,10 @@ public class ColorManager {
     
     // MARK: Initializers
     
-    public init(rgba: RGBAColor) {
-        // Initialize RGBA color
+    public init(color: UIColor) {
+        // Initialize color
         
-        self._rgba = rgba
+        self._color = color
     }
     
     // MARK: Deinitializer
@@ -31,59 +31,21 @@ public class ColorManager {
     
     // MARK: Object variables & properties
     
-    fileprivate var _rgba: RGBAColor!
-    
-    public var rgba: RGBAColor {
-        get {
-            return self._rgba
-        }
-    }
+    fileprivate var _color: UIColor!
     
     public var color: UIColor {
         get {
-            return UIColor(rgbaColor: self.rgba)
+            return self._color
+        }
+    }
+    
+    public var rgba: RGBAColor {
+        get {
+            return RGBAColor.from(color: self.color)
         }
     }
     
     // MARK: Public object methods
-    
-    @discardableResult
-    public func invert(invertAlpha: Bool) -> ColorManager {
-        let invertedRGBAColor = RGBAColor(
-            red: RGBAComponent(cgFloatValue: 1.0 - self.rgba.red.cgFloatValue),
-            green: RGBAComponent(cgFloatValue: 1.0 - self.rgba.green.cgFloatValue),
-            blue: RGBAComponent(cgFloatValue: 1.0 - self.rgba.blue.cgFloatValue),
-            alpha: RGBAComponent(cgFloatValue: invertAlpha ? 1.0 - self.rgba.red.cgFloatValue : self.rgba.alpha.cgFloatValue)
-        )
-        
-        return ColorManager(rgba: invertedRGBAColor)
-    }
-    
-    @discardableResult
-    public func mix(with otherColor: RGBAColor) -> ColorManager {
-        let mixedRGBAColor = RGBAColor(
-            red: RGBAComponent(
-                cgFloatValue: (self.rgba.red.cgFloatValue + otherColor.red.cgFloatValue) / 2.0
-            ),
-            green: RGBAComponent(
-                cgFloatValue: (self.rgba.green.cgFloatValue + otherColor.green.cgFloatValue) / 2.0
-            ),
-            blue: RGBAComponent(
-                cgFloatValue: (self.rgba.blue.cgFloatValue + otherColor.blue.cgFloatValue) / 2.0
-            ),
-            alpha: RGBAComponent(
-                cgFloatValue: (self.rgba.alpha.cgFloatValue + otherColor.alpha.cgFloatValue) / 2.0
-            )
-        )
-        
-        return ColorManager(rgba: mixedRGBAColor)
-    }
-    
-    @discardableResult
-    public func mix(with otherColor: UIColor) -> ColorManager {
-        let otherRGBAColor = RGBAColor.from(color: otherColor)
-        return self.mix(with: otherRGBAColor)
-    }
     
     // MARK: Private object methods
     
