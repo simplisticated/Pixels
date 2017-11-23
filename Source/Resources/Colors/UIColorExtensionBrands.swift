c50e1a25a623b0ae306dc5589849bfe89a6ec0e8
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-11-23 06:00:37 +0300

    Added HTML colors

---
+++ Source/Resources/Colors/UIColorExtensionBrands.swift
@@ -0,0 +1,54 @@
+//
+//  UIColorExtensionBrands.swift
+//  Pixels
+//
+//  Created by Igor Matyushkin on 13.06.16.
+//  Copyright © 2016 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public extension UIColor {
+    
+    public struct Brands {
+        
+        public struct Facebook {
+            public static let blue = UIColor(hexString: "#3b5998")!
+        }
+        
+        public struct Flickr {
+            public static let blue = UIColor(hexString: "#0063dc")!
+            public static let pink = UIColor(hexString: "#ff0084")!
+        }
+        
+        public struct Google {
+            public static let blue = UIColor(hexString: "#4285f4")!
+            public static let green = UIColor(hexString: "#34a853")!
+            public static let yellow = UIColor(hexString: "#fbbc05")!
+            public static let red = UIColor(hexString: "#ea4335")!
+        }
+        
+        public struct Netflix {
+            public static let red = UIColor(hexString: "#e50914")!
+            public static let black = UIColor(hexString: "#221f1f")!
+        }
+        
+        public struct Nvidia {
+            public static let green = UIColor(hexString: "#76b900")!
+        }
+        
+        public struct Skype {
+            public static let blue = UIColor(hexString: "#00aff0")!
+        }
+        
+        public struct Slack {
+            public static let blue = UIColor(hexString: "#6ecadc")!
+            public static let yellow = UIColor(hexString: "#e9a820")!
+            public static let red = UIColor(hexString: "#e01563")!
+            public static let green = UIColor(hexString: "#3eb991")!
+        }
+        
+    }
+    
+}
---
+++ Source/Resources/Colors/UIColorExtensionHTMLColors.swift
@@ -0,0 +1,165 @@
+//
+//  UIColorExtensionHTMLColors.swift
+//  Pixels
+//
+//  Created by Igor Matyushkin on 23.11.2017.
+//  Copyright © 2017 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public extension UIColor {
+    
+    public struct HTML {
+        public static let aliceBlue = UIColor(hexString: "#F0F8FF")!
+        public static let antiqueWhite = UIColor(hexString: "#FAEBD7")!
+        public static let aqua = UIColor(hexString: "#00FFFF")!
+        public static let aquamarine = UIColor(hexString: "#7FFFD4")!
+        public static let azure = UIColor(hexString: "#F0FFFF")!
+        public static let beige = UIColor(hexString: "#F5F5DC")!
+        public static let bisque = UIColor(hexString: "#FFE4C4")!
+        public static let black = UIColor(hexString: "#000000")!
+        public static let blanchedAlmond = UIColor(hexString: "#FFEBCD")!
+        public static let blue = UIColor(hexString: "#0000FF")!
+        public static let blueViolet = UIColor(hexString: "#8A2BE2")!
+        public static let brown = UIColor(hexString: "#A52A2A")!
+        public static let burlyWood = UIColor(hexString: "#DEB887")!
+        public static let cadetBlue = UIColor(hexString: "#5F9EA0")!
+        public static let chartreuse = UIColor(hexString: "#7FFF00")!
+        public static let chocolate = UIColor(hexString: "#D2691E")!
+        public static let coral = UIColor(hexString: "#FF7F50")!
+        public static let cornflowerBlue = UIColor(hexString: "#6495ED")!
+        public static let cornsilk = UIColor(hexString: "#FFF8DC")!
+        public static let crimson = UIColor(hexString: "#DC143C")!
+        public static let cyan = UIColor(hexString: "#00FFFF")!
+        public static let darkBlue = UIColor(hexString: "#00008B")!
+        public static let darkCyan = UIColor(hexString: "#008B8B")!
+        public static let darkGoldenRod = UIColor(hexString: "#B8860B")!
+        public static let darkGray = UIColor(hexString: "#A9A9A9")!
+        public static let darkGrey = UIColor(hexString: "#A9A9A9")!
+        public static let darkGreen = UIColor(hexString: "#006400")!
+        public static let darkKhaki = UIColor(hexString: "#BDB76B")!
+        public static let darkMagenta = UIColor(hexString: "#8B008B")!
+        public static let darkOliveGreen = UIColor(hexString: "#556B2F")!
+        public static let darkOrange = UIColor(hexString: "#FF8C00")!
+        public static let darkOrchid = UIColor(hexString: "#9932CC")!
+        public static let darkRed = UIColor(hexString: "#8B0000")!
+        public static let darkSalmon = UIColor(hexString: "#E9967A")!
+        public static let darkSeaGreen = UIColor(hexString: "#8FBC8F")!
+        public static let darkSlateBlue = UIColor(hexString: "#483D8B")!
+        public static let darkSlateGray = UIColor(hexString: "#2F4F4F")!
+        public static let darkSlateGrey = UIColor(hexString: "#2F4F4F")!
+        public static let darkTurquoise = UIColor(hexString: "#00CED1")!
+        public static let darkViolet = UIColor(hexString: "#9400D3")!
+        public static let deepPink = UIColor(hexString: "#FF1493")!
+        public static let deepSkyBlue = UIColor(hexString: "#00BFFF")!
+        public static let dimGray = UIColor(hexString: "#696969")!
+        public static let dimGrey = UIColor(hexString: "#696969")!
+        public static let dodgerBlue = UIColor(hexString: "#1E90FF")!
+        public static let fireBrick = UIColor(hexString: "#B22222")!
+        public static let floralWhite = UIColor(hexString: "#FFFAF0")!
+        public static let forestGreen = UIColor(hexString: "#228B22")!
+        public static let fuchsia = UIColor(hexString: "#FF00FF")!
+        public static let gainsboro = UIColor(hexString: "#DCDCDC")!
+        public static let ghostWhite = UIColor(hexString: "#F8F8FF")!
+        public static let gold = UIColor(hexString: "#FFD700")!
+        public static let goldenRod = UIColor(hexString: "#DAA520")!
+        public static let gray = UIColor(hexString: "#808080")!
+        public static let grey = UIColor(hexString: "#808080")!
+        public static let green = UIColor(hexString: "#008000")!
+        public static let greenYellow = UIColor(hexString: "#ADFF2F")!
+        public static let honeyDew = UIColor(hexString: "#F0FFF0")!
+        public static let hotPink = UIColor(hexString: "#FF69B4")!
+        public static let indianRed = UIColor(hexString: " #CD5C5C")!
+        public static let indigo = UIColor(hexString: " #4B0082")!
+        public static let ivory = UIColor(hexString: "#FFFFF0")!
+        public static let khaki = UIColor(hexString: "#F0E68C")!
+        public static let lavender = UIColor(hexString: "#E6E6FA")!
+        public static let lavenderBlush = UIColor(hexString: "#FFF0F5")!
+        public static let lawnGreen = UIColor(hexString: "#7CFC00")!
+        public static let lemonChiffon = UIColor(hexString: "#FFFACD")!
+        public static let lightBlue = UIColor(hexString: "#ADD8E6")!
+        public static let lightCoral = UIColor(hexString: "#F08080")!
+        public static let lightCyan = UIColor(hexString: "#E0FFFF")!
+        public static let lightGoldenRodYellow = UIColor(hexString: "#FAFAD2")!
+        public static let lightGray = UIColor(hexString: "#D3D3D3")!
+        public static let lightGrey = UIColor(hexString: "#D3D3D3")!
+        public static let lightGreen = UIColor(hexString: "#90EE90")!
+        public static let lightPink = UIColor(hexString: "#FFB6C1")!
+        public static let lightSalmon = UIColor(hexString: "#FFA07A")!
+        public static let lightSeaGreen = UIColor(hexString: "#20B2AA")!
+        public static let lightSkyBlue = UIColor(hexString: "#87CEFA")!
+        public static let lightSlateGray = UIColor(hexString: "#778899")!
+        public static let lightSlateGrey = UIColor(hexString: "#778899")!
+        public static let lightSteelBlue = UIColor(hexString: "#B0C4DE")!
+        public static let lightYellow = UIColor(hexString: "#FFFFE0")!
+        public static let lime = UIColor(hexString: "#00FF00")!
+        public static let limeGreen = UIColor(hexString: "#32CD32")!
+        public static let linen = UIColor(hexString: "#FAF0E6")!
+        public static let magenta = UIColor(hexString: "#FF00FF")!
+        public static let maroon = UIColor(hexString: "#800000")!
+        public static let mediumAquaMarine = UIColor(hexString: "#66CDAA")!
+        public static let mediumBlue = UIColor(hexString: "#0000CD")!
+        public static let mediumOrchid = UIColor(hexString: "#BA55D3")!
+        public static let mediumPurple = UIColor(hexString: "#9370DB")!
+        public static let mediumSeaGreen = UIColor(hexString: "#3CB371")!
+        public static let mediumSlateBlue = UIColor(hexString: "#7B68EE")!
+        public static let mediumSpringGreen = UIColor(hexString: "#00FA9A")!
+        public static let mediumTurquoise = UIColor(hexString: "#48D1CC")!
+        public static let mediumVioletRed = UIColor(hexString: "#C71585")!
+        public static let midnightBlue = UIColor(hexString: "#191970")!
+        public static let mintCream = UIColor(hexString: "#F5FFFA")!
+        public static let mistyRose = UIColor(hexString: "#FFE4E1")!
+        public static let moccasin = UIColor(hexString: "#FFE4B5")!
+        public static let navajoWhite = UIColor(hexString: "#FFDEAD")!
+        public static let navy = UIColor(hexString: "#000080")!
+        public static let oldLace = UIColor(hexString: "#FDF5E6")!
+        public static let olive = UIColor(hexString: "#808000")!
+        public static let oliveDrab = UIColor(hexString: "#6B8E23")!
+        public static let orange = UIColor(hexString: "#FFA500")!
+        public static let orangeRed = UIColor(hexString: "#FF4500")!
+        public static let orchid = UIColor(hexString: "#DA70D6")!
+        public static let paleGoldenRod = UIColor(hexString: "#EEE8AA")!
+        public static let paleGreen = UIColor(hexString: "#98FB98")!
+        public static let paleTurquoise = UIColor(hexString: "#AFEEEE")!
+        public static let paleVioletRed = UIColor(hexString: "#DB7093")!
+        public static let papayaWhip = UIColor(hexString: "#FFEFD5")!
+        public static let peachPuff = UIColor(hexString: "#FFDAB9")!
+        public static let peru = UIColor(hexString: "#CD853F")!
+        public static let pink = UIColor(hexString: "#FFC0CB")!
+        public static let plum = UIColor(hexString: "#DDA0DD")!
+        public static let powderBlue = UIColor(hexString: "#B0E0E6")!
+        public static let purple = UIColor(hexString: "#800080")!
+        public static let rebeccaPurple = UIColor(hexString: "#663399")!
+        public static let red = UIColor(hexString: "#FF0000")!
+        public static let rosyBrown = UIColor(hexString: "#BC8F8F")!
+        public static let royalBlue = UIColor(hexString: "#4169E1")!
+        public static let saddleBrown = UIColor(hexString: "#8B4513")!
+        public static let salmon = UIColor(hexString: "#FA8072")!
+        public static let sandyBrown = UIColor(hexString: "#F4A460")!
+        public static let seaGreen = UIColor(hexString: "#2E8B57")!
+        public static let seaShell = UIColor(hexString: "#FFF5EE")!
+        public static let sienna = UIColor(hexString: "#A0522D")!
+        public static let silver = UIColor(hexString: "#C0C0C0")!
+        public static let skyBlue = UIColor(hexString: "#87CEEB")!
+        public static let slateBlue = UIColor(hexString: "#6A5ACD")!
+        public static let slateGray = UIColor(hexString: "#708090")!
+        public static let slateGrey = UIColor(hexString: "#708090")!
+        public static let snow = UIColor(hexString: "#FFFAFA")!
+        public static let springGreen = UIColor(hexString: "#00FF7F")!
+        public static let steelBlue = UIColor(hexString: "#4682B4")!
+        public static let tan = UIColor(hexString: "#D2B48C")!
+        public static let teal = UIColor(hexString: "#008080")!
+        public static let thistle = UIColor(hexString: "#D8BFD8")!
+        public static let tomato = UIColor(hexString: "#FF6347")!
+        public static let turquoise = UIColor(hexString: "#40E0D0")!
+        public static let violet = UIColor(hexString: "#EE82EE")!
+        public static let wheat = UIColor(hexString: "#F5DEB3")!
+        public static let white = UIColor(hexString: "#FFFFFF")!
+        public static let whiteSmoke = UIColor(hexString: "#F5F5F5")!
+        public static let yellow = UIColor(hexString: "#FFFF00")!
+        public static let yellowGreen = UIColor(hexString: "#9ACD32")!
+    }
+    
+}
