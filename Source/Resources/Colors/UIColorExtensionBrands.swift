6f363fc68a4a64adc01ad8be8c34eebc6a0bcd0e
Igor Matyushkin <igormatyushkin014@gmail.com>
2018-05-09 19:28:49 +0300

    Updated brand colors

--- Source/Resources/Colors/UIColorExtensionBrands.swift
+++ Source/Resources/Colors/UIColorExtensionBrands.swift
@@ -14,39 +14,121 @@ public extension UIColor {
     public struct Brands {
         
         public struct Facebook {
-            public static let blue = UIColor(hexString: "#3b5998")!
+            
+            public static var blue: UIColor {
+                get {
+                    return UIColor(hexString: "#3b5998")!
+                }
+            }
+            
         }
         
         public struct Flickr {
-            public static let blue = UIColor(hexString: "#0063dc")!
-            public static let pink = UIColor(hexString: "#ff0084")!
+            
+            public static var blue: UIColor {
+                get {
+                    return UIColor(hexString: "#0063dc")!
+                }
+            }
+            
+            public static var pink: UIColor {
+                get {
+                    return UIColor(hexString: "#ff0084")!
+                }
+            }
+            
         }
         
         public struct Google {
-            public static let blue = UIColor(hexString: "#4285f4")!
-            public static let green = UIColor(hexString: "#34a853")!
-            public static let yellow = UIColor(hexString: "#fbbc05")!
-            public static let red = UIColor(hexString: "#ea4335")!
+            
+            public static var blue: UIColor {
+                get {
+                    return UIColor(hexString: "#4285f4")!
+                }
+            }
+            
+            public static var green: UIColor {
+                get {
+                    return UIColor(hexString: "#34a853")!
+                }
+            }
+            
+            public static var yellow: UIColor {
+                get {
+                    return UIColor(hexString: "#fbbc05")!
+                }
+            }
+            
+            public static var red: UIColor {
+                get {
+                    return UIColor(hexString: "#ea4335")!
+                }
+            }
+            
         }
         
         public struct Netflix {
-            public static let red = UIColor(hexString: "#e50914")!
-            public static let black = UIColor(hexString: "#221f1f")!
+            
+            public static var red: UIColor {
+                get {
+                    return UIColor(hexString: "#e50914")!
+                }
+            }
+            
+            public static var black: UIColor {
+                get {
+                    return UIColor(hexString: "#221f1f")!
+                }
+            }
+            
         }
         
         public struct Nvidia {
-            public static let green = UIColor(hexString: "#76b900")!
+            
+            public static var green: UIColor {
+                get {
+                    return UIColor(hexString: "#76b900")!
+                }
+            }
+            
         }
         
         public struct Skype {
-            public static let blue = UIColor(hexString: "#00aff0")!
+            
+            public static var blue: UIColor {
+                get {
+                    return UIColor(hexString: "#00aff0")!
+                }
+            }
+            
         }
         
         public struct Slack {
-            public static let blue = UIColor(hexString: "#6ecadc")!
-            public static let yellow = UIColor(hexString: "#e9a820")!
-            public static let red = UIColor(hexString: "#e01563")!
-            public static let green = UIColor(hexString: "#3eb991")!
+            
+            public static var blue: UIColor {
+                get {
+                    return UIColor(hexString: "#6ecadc")!
+                }
+            }
+            
+            public static var yellow: UIColor {
+                get {
+                    return UIColor(hexString: "#e9a820")!
+                }
+            }
+            
+            public static var red: UIColor {
+                get {
+                    return UIColor(hexString: "#e01563")!
+                }
+            }
+            
+            public static var green: UIColor {
+                get {
+                    return UIColor(hexString: "#3eb991")!
+                }
+            }
+            
         }
         
     }
